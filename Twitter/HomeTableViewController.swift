53dfbd85a7dfb53ba97838635d4c27b27d2283d9
Saras Paudel <saraspaudel@utexas.edu>
2019-02-26 23:21:18 -0600

    Added user's photo

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -53,6 +53,13 @@ class HomeTableViewController: UITableViewController {
         cell.userNameLabel.text = user["name"] as? String
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
         
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data{
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
         return cell
     }
 
